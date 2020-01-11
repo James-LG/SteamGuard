177d35a534b84aa38a4139563180655601964e5a
James La Novara-Gsell <james.lanovara.gsell@gmail.com>
2020-01-10 23:55:37 -0500

    Change requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="steamguard-Tygon", # Replace with your own username
+    version="0.0.1",
+    author="Tygon",
+    author_email="",
+    description="A small example package",
+    url="https://github.com/Tygon/SteamGuard",
+)
